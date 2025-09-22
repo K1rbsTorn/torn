726cc883ce82b4ca5591298d22468514c48a7313
K1rbsTorn <k1rbs.torn@gmail.com>
2025-09-22 23:55:40 +0200

    Update racing.user.js
    Random Colours for Everyone

--- racing.user.js
+++ racing.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Torn: Racing enhancements Custom Skin
 // @namespace    lugburz.racing_enhancements
-// @version      0.5.27
+// @version      0.5.30
 // @description  Show car's current speed, precise skill, official race penalty, racing skill of others and race car skins (with custom overrides).
 // @author       Lugburz & K1rbs
 // @match        https://www.torn.com/*
@@ -39,29 +39,241 @@ const SHOW_SKINS = GM_getValue('showSkinsChk') != 0;
 const SKIN_AWARDS = 'https://race-skins.brainslug.nl/custom/data';
 const SKIN_IMAGE = id => `https://race-skins.brainslug.nl/assets/${id}`;
 
-// --- START: Custom Skin Configuration (NEW) ---
+// --- START: Custom Skin Configuration (MODIFIED) ---
 // This section allows you to override skins for specific users and cars.
 const CUSTOM_SKIN_CONFIG = {
     // Add user IDs to this list. The rules below will apply to them.
     userIds: [3090251, 2958211], // Example User IDs
 
     // Define which skin to use for which car ID.
-    // Example: A Honda NSX has item ID 78. Find the car ID on the item market page.
-    // 'CarItemID': 'SkinID',
+    // You can use a standard Skin ID OR a full URL to your own image.
     skinsByCarId: {
-        '78': 'e9c95171-a85f-4874-8a49-25ddf536d2aa',
+        // This is a STANDARD skin from brainslug.nl
+        '522': '0661b541-eca4-4c75-9914-35daa85c9651',
+
+        // This is YOUR CUSTOM skin from your own URL
+        '78': 'https://i.imgur.com/your-image-link.png', // <-- REPLACE WITH YOUR IMAGE URL
+
         '511': '9e464fa6-275c-44ae-8d50-4648a13f9a46',
         '85': 'b2a8b231-d385-4f76-966a-307e8ad956d1',
         '517': '094b898e-a197-4fdd-a6ff-d2ab88e00a4e',
         '518': 'fc130f42-6fe1-4bdf-98e1-f1207ed04153',
         '520': '7004b0bc-5750-45e4-be3a-496afb9370b2',
         '521': '6d533149-3ac5-4209-b7dc-1337987104d4',
-        '522': '0661b541-eca4-4c75-9914-35daa85c9651',
         '523': 'ddc24665-e41a-44c0-9f3a-3eacb3308f13',
         '524': '3b642eb4-e774-4c50-a3eb-1beabcf65392',
     }
 };
 // --- END: Custom Skin Configuration ---
+
+// --- START: Random Skin Pool Configuration (NEW) ---
+// Skins here will be randomly applied to players who do NOT have an official skin
+// and are NOT in your 'CUSTOM_SKIN_CONFIG.userIds' list above.
+// Group skin IDs in arrays, keyed by the car's item ID.
+const RANDOM_SKIN_POOL = {
+    // Example for Honda NSX (Car ID '78')
+    '78': [
+        'e9c95171-a85f-4874-8a49-25ddf536d2aa',
+        '6d736e53-95fe-493e-8e11-8649865f4a59',
+        '2ad80d5d-3b06-47da-9cf8-274a20814a8e',
+        'e787b7f6-7bae-43eb-ae7a-5da9f8f17d8b',
+        '4be186df-898d-45f0-abe5-319192e83ab3',
+        '313e33ed-70e4-4b1a-8b72-95b6357cd662',
+        '5fcd00ff-002a-4a91-b10c-52cbfc931169',
+        '6a88e7d7-8fe8-42b9-9ba6-c6de282907e0',
+        '7525290f-c093-45d5-9415-c61a67dd8ee1',
+        'fa87e13e-b297-4c20-b8a0-c17d96c97924',
+        '2c701bdb-a9af-4f23-b3c7-91887afee448',
+        '13d721e0-8de2-417f-9166-f27a924a66a4',
+        '517c0899-75e5-4bf2-9521-d57d9c53e8a5',
+        '8cb63bbf-2126-4987-9699-71e3b4f2d501',
+        '6f4f32ff-dc08-41f0-a352-3f10cef16321',
+        'ffe6dd68-7361-4c59-8fed-749ae9219438',
+    ],
+    '85': [
+        'cf2e3540-87b0-49d7-9e6f-c7491ba81477',
+        'd63e4535-e581-456b-8740-f7ec070c6f7a',
+        '12d9e38a-b34b-46ce-93b4-a6c373b41e3d',
+        '5fe1005d-9778-45b6-87da-09795ab155a2',
+        '197c7525-c9b9-4bc4-bab8-a4a667b5555d',
+        'aca01078-cd01-4c41-83ad-22e11349a3b4',
+        'b2a8b231-d385-4f76-966a-307e8ad956d1',
+        '357e4bbf-90f5-44f8-9f38-189fc5abb2fa',
+        '55efcaf7-a407-417d-9e06-1dd5bf4917d9',
+        '2596e071-e933-43b6-be5b-152f1c066f33',
+        '483590cb-e5ae-46eb-92a8-181b645d89cd',
+        'ffd296c6-98cd-4b35-91dd-41b86fef9ccb',
+    ],
+    '94': [
+        '582d1932-f8ea-4a00-ba8c-74a6bece78a9',
+        '2ae4c9fe-094d-4f2e-b071-38707869a65a',
+    ],
+    '494': [
+        '0961458b-8cee-48bb-b09f-895dbfe9136f',
+        '1a8fe10e-95eb-42ae-a1eb-4dee8180f1d0',
+        '93b3adaf-163f-4a79-9102-4b900501c7b9',
+        '01c6a129-3004-403c-803e-f5fa63d5cad2',
+    ],
+    '511': [
+        '9182c0cc-23bf-46d0-83ab-f939b2f07072',
+        '34bf1078-8f04-4047-b40e-870380f6f41a',
+        'c47c751a-14be-41a0-a836-9212223c1aa9',
+        'f0f8e29d-0014-43ab-8b13-93092faaeb2d',
+        '9e464fa6-275c-44ae-8d50-4648a13f9a46',
+        '10e108f2-1087-4de6-b2c3-20d7a979f4d5',
+        '94be2d2f-310b-4031-9bfc-b4927ef952fd',
+        'ce8334bf-dc63-4459-903d-80e14c8fa64f',
+        '6462b7d4-e6b0-493f-a15d-d5e5ca04bc93',
+        '356d22b6-2f35-4b35-8874-89d73912a0f9',
+        '729107a2-755e-43f3-85dc-e33e5e68d83d',
+        '935cc150-fda8-46f3-87b9-0c0fa03837a9',
+        'e23754c4-c4b1-4a90-9007-237bbed10759',
+        '0e1f9c11-98dc-40c9-823c-570e0f6b2a28',
+        '65cedda0-5a21-4ddf-adea-5e1f41636b06',
+        'ae2fb989-4725-4e57-943a-ba54e5fa20cb',
+        '9248d09e-f64b-410f-b62a-b5e2c7121201',
+    ],
+    '517': [
+        'ac68ec57-bd26-49b2-aad2-dadc5f1cae9b',
+        'ef70622d-b569-4a51-a99a-9ef29eda0ddd',
+        'e5fe72b1-e477-4a7a-b387-ddcf83cbc795',
+        '924b59b0-99a3-47d3-b1c0-645b089e10e5',
+        'edd08993-929a-4530-94a5-0a2b22d2aa20',
+        '586ea3f5-1f67-4161-a705-9b9f3247de60',
+        '094b898e-a197-4fdd-a6ff-d2ab88e00a4e',
+        '9bbf8881-dad9-4c3a-a012-033fcceac288',
+        'ed256792-caa6-4a9a-a6ed-0beeccdac699',
+        '69c4f6ef-2bdd-48cf-8385-0a2cc4bda15a',
+        '1becc487-3590-4340-9d01-a084922182e2',
+        '39ae6daa-1c62-4b90-9747-ca35dd29b044',
+        '2d6eae50-37c6-4358-b56a-6164c4a9e650',
+    ],
+    '518': [
+        'ac1eadcd-65dd-4e11-a850-794385f0f7fc',
+        '15ef0a58-3582-487c-ba3f-c5736e72a3a0',
+        '8ac463aa-9981-46e5-b1d0-cf063b22905e',
+        '78c3e4e5-881c-4946-a4d3-15c3bfca2f69',
+        '098ebbc5-690e-4420-944f-447fa2d18aa8',
+        '59d3b4a5-e074-49e4-82f4-93e85bd1ac0d',
+        '210edd4d-99d8-4687-a20a-2bfb2489dc9f',
+        'f93b57cf-8c6a-4b30-a06c-d261387e8885',
+        'e7228c67-f8e3-44da-96b8-50cc9699b66e',
+        'd685083a-86ab-4980-a6cf-2e3b79083c91',
+        'dc5ed805-eff9-45b0-b24c-0dfffc7cf9f3',
+        '8092df84-6616-4a3c-84db-236a0a03b9ce',
+        '5f5d1f89-5522-46d4-aa1c-a0b6c4fede02',
+        'e94d38ad-8231-43ac-a7e8-85270605ce44',
+        '41704e59-5aea-4e20-bb8f-fd1e21e9d8c3',
+        'fc130f42-6fe1-4bdf-98e1-f1207ed04153',
+        '6bd5ec69-b33a-4b2c-aaff-543e0d8bdb54',
+    ],
+    '519': [
+        'c8c3e7f4-b7f5-4d71-abb2-9641a7c1b97f',
+        'a8950213-5642-4c56-97ca-948d11ed9e89',
+        'b3de65d5-a000-4c5b-a44c-1c726c3dc6d7',
+        'e56c9806-dac9-47eb-9ad9-ed9a8131f12d',
+        'f74b255f-03a5-4182-8fdd-91a5a7195dfa',
+        '03f86a55-68a9-40d9-bb4e-f1231215bc3e',
+        'bbe754bd-b3c5-4052-ba0b-83feac4a41bc',
+        '54990683-8ba6-4aa8-900f-68f9fbe01efe',
+        '381ad65e-9b53-4d66-b05d-e6724af8a86d',
+        '3b11f8ea-59f1-436f-866e-b46c174eb47c',
+        '8be0d251-9acd-4a20-ac39-82ad148d9dc0',
+        '9b94ff4f-d2a8-4c5c-9e8d-c99c7264040a',
+    ],
+    '520': [
+        'bb4fd6d4-0914-4a1d-bce0-198d88120c05',
+        '6c202d23-4c45-4a9a-8b05-465cafd1e163',
+        'ad6ec240-28b4-4e8f-8ab6-089c386f4c39',
+        'd012826a-9620-4ae0-89ab-808d365375e4',
+        'd0c19c7b-286a-43d5-a97c-7820574c7538',
+        'fa0098cb-6188-4a0a-9324-9263554cafe3',
+        'a191aca4-5fa7-4dd9-879d-1d580996e305',
+        'c424dcb8-58b8-4922-a647-c517d29d9f73',
+        '64cca8c3-ece1-451a-bb64-6cd71cd978d0',
+        '56fecf88-07b3-40ab-85be-bcbbb268ab4d',
+        '32d6205d-9180-4300-9b1e-d5c2c03d2e4b',
+        '2ec315b7-218b-4fdf-96dd-0b51b8b5649e',
+        '7004b0bc-5750-45e4-be3a-496afb9370b2',
+        'd9e8329a-d74b-4778-a82f-a737f238097b',
+        '80069b12-32d7-4c16-a0a9-86022b1f64b9',
+        '221a0090-3028-42bb-a973-5a387c595f0b',
+        '990e55f8-9a5b-4ce9-b2e6-dfb6cc3cf17f',
+    ],
+    '521': [
+        'e2bdc3c5-bcc2-496b-aff2-e6e78d52efc8',
+        '4196627f-9cf6-49b4-af0d-71ae4099589c',
+        '6d533149-3ac5-4209-b7dc-1337987104d4',
+        'fe59b80d-2a29-44f8-977b-739e7d747a72',
+        '1acd4ebf-e268-491e-a31e-943aa96a96da',
+        '5a3c4f96-1729-4217-b3ad-29685a038f08',
+        'd12a38fa-35de-4e67-aa14-02ed23cc1671',
+        '66bafbe7-2488-46fc-943f-d385a7382145',
+        '1aa81c08-32ee-4ec6-8aa0-6243d8ad5aae',
+        '1ddb031d-93db-40eb-96ec-a1ad62df35e2',
+        'd4a0204a-d32e-4aac-85d3-8b6dd011a4cb',
+        'f52792b6-74f6-45c7-9c63-fb415160a0e6',
+        '818d5b29-c078-45b2-ba00-17ff43ffb7f3',
+        '9eabebd2-1d5b-4144-bff3-7c6906c35ff5',
+        '52095f12-b634-4b03-bd92-c8595e437774',
+    ],
+    '522': [
+        '1d3f30a6-af4d-4549-ab20-52c8bbb09afc',
+        'cb761b2a-9b1a-4b58-a3ad-0250857b9c1e',
+        'a814ed60-2e04-43ca-9631-f0e96652b7e9',
+        '679101df-c21c-4781-bd9c-8b6ba78ef06c',
+        '7e221801-7804-4399-b678-915e777e84fb',
+        '0661b541-eca4-4c75-9914-35daa85c9651',
+        '2ab6b330-80e4-4d58-9a5b-2c581539ebb3',
+        'a8ae03d9-21bb-4c71-a078-4f6652bb21df',
+        '8b24ba9e-6d21-4694-a06f-5d7cb5eb2cbe',
+        'aea1c273-06c2-4f4e-97e3-88d447f1f5d2',
+        '5bc96008-3a1a-4161-b7e8-438787f6bae8',
+        '345c9236-4d15-4c3a-b465-b4c95c3bdbb4',
+        '3830c3e5-0033-4795-8751-46b5a5bf8608',
+        'd7fa9c91-275f-43b9-9e5c-6bbc27d32f4a',
+        'f5686b4d-3c29-4d64-a9c1-fa1a9f858ff8',
+        '5c9bc336-7cfe-43b8-a0e6-7715867f0896',
+        '15c1df4c-92e2-465e-ab73-a9e4147a622f',
+    ],
+    '523': [
+        '08ab3186-556d-4c6a-9b06-f88a6305ec7a',
+        'ddc24665-e41a-44c0-9f3a-3eacb3308f13',
+        'cfc741be-832c-4241-9780-81ab516657f7',
+        'e7e7208e-0aa2-4d2d-8632-fbd019de5b4a',
+        '39d81d7f-5d61-4c78-b7fa-3abae0e6d3e0',
+        'e6fff80e-0381-4c3f-ae75-5fdbbf152e7a',
+        '37f094c4-4292-4021-b364-5bdd4d0a7f93',
+        '45c84a0e-7fa6-4005-9feb-32b05ccfa0a7',
+        '38585a97-c00f-408c-9f57-0166563664b8',
+        '66683fd8-5d77-49ee-a078-1916fa604c51',
+        '3130df02-e0bb-42a3-b0fd-27bd0dac8756',
+        '46a91466-b33a-4fbb-bf58-ac5e1dfc777a',
+        '8806734e-2a76-4fb3-a3b4-9edd76134c5a',
+    ],
+    '524': [
+        '9c8bcfc3-3222-4048-b62b-76733bb24172',
+        'ba42867d-69c5-4921-88c7-73e9c752af46',
+        '3b642eb4-e774-4c50-a3eb-1beabcf65392',
+        '6cfaa57b-cc03-42f9-802d-045aeceb30c1',
+        'e7685014-199a-4a29-ad6e-ecb8e29ed2db',
+        'eed9e7b7-68fb-438c-b469-23197a511c1f',
+        'a369abf7-7c55-4c0b-a68c-cc696cffad31',
+        '93939b43-15d7-4695-8ef0-fc237f124a80',
+        'c110e28a-bdd5-4c7e-a165-3be9f061ffee',
+        'aabceb04-3b9e-4d7d-a9e7-2421896a7b76',
+        'aca59f86-79a0-42fd-b548-b8011735b220',
+        '23c61be7-33c7-489f-8c96-44496e4c71e2',
+        '88299689-c48a-4cda-b0d3-b0aee7a670db',
+        '5c05dd42-51d8-4806-a975-1470f0911621',
+        'd2d876b5-93de-4e70-8b4a-64ea57d5a104',
+        'ed89de12-e3ea-4b3c-adfb-228256dea494',
+        'cd790a59-6e50-49fc-8a14-269203fb8ccd',
+    ],
+    // Add more car IDs and skin ID arrays as you wish
+    // 'CarID': [ 'SkinID1', 'SkinID2', ... ],
+};
+// --- END: Random Skin Pool Configuration ---
 
 const userID = getUserIdFromCookie();
 var RACE_ID = '*';
@@ -125,20 +337,21 @@ async function updateDriversList() {
             }
         }
 
-        // --- Combined Skin Logic (MODIFIED) ---
+        // --- Combined Skin Logic (MODIFIED FOR RANDOMIZATION) ---
         if (SHOW_SKINS) {
             const carImg = driver.querySelector('.car')?.querySelector('img');
             if (!carImg) continue; // Skip if there's no car image element
 
             const carId = carImg.getAttribute('src').replace(/[^0-9]*/g, '');
-            let customSkinApplied = false;
+            const userHasOfficialSkin = !!racingSkins[driverId] && !!racingSkins[driverId][carId];
 
-            // 1. Check for and apply your local "assertive" custom skins first.
+            // Priority 1: Apply your assertive custom skins for users in 'userIds'.
             if (CUSTOM_SKIN_CONFIG.userIds.includes(driverId)) {
                 const customSkinForThisCar = CUSTOM_SKIN_CONFIG.skinsByCarId[carId];
                 if (customSkinForThisCar) {
-                    const customSkinUrl = SKIN_IMAGE(customSkinForThisCar);
-                    customSkinApplied = true;
+                    const customSkinUrl = customSkinForThisCar.startsWith('http')
+                        ? customSkinForThisCar
+                        : SKIN_IMAGE(customSkinForThisCar);
 
                     // Apply the skin and set up a guard to re-apply it if it gets overwritten.
                     if (carImg.getAttribute('src') !== customSkinUrl) {
@@ -148,7 +361,7 @@ async function updateDriversList() {
                         carImg.dataset.skinWatcher = 'true';
                         const watcher = new MutationObserver(() => {
                             if (carImg.getAttribute('src') !== customSkinUrl) {
-                                watcher.disconnect(); // Temporarily stop watching to prevent loops
+                                watcher.disconnect();
                                 carImg.setAttribute('src', customSkinUrl);
                                 watcher.observe(carImg, { attributes: true, attributeFilter: ['src'] });
                             }
@@ -161,13 +374,20 @@ async function updateDriversList() {
                     }
                 }
             }
-
-            // 2. If no custom override was applied, apply the regular server-fetched skin.
-            if (!customSkinApplied && !!racingSkins[driverId] && !!racingSkins[driverId][carId]) {
-                // Only apply if the assertive watcher isn't present to avoid conflicts.
+            // Priority 2: Apply the official server-fetched skin.
+            else if (userHasOfficialSkin) {
                 if (!carImg.dataset.skinWatcher) {
                     carImg.setAttribute('src', SKIN_IMAGE(racingSkins[driverId][carId]));
                     if (driverId == userID) skinCarSidebar(racingSkins[driverId][carId]);
+                }
+            }
+            // Priority 3: Apply a consistent "random" skin from your pool.
+            else {
+                const skinPool = RANDOM_SKIN_POOL[carId];
+                if (skinPool && skinPool.length > 0) {
+                    // (MODIFIED) Use the driver's ID to pick a consistent skin.
+                    const randomSkinId = skinPool[driverId % skinPool.length];
+                    carImg.setAttribute('src', SKIN_IMAGE(randomSkinId));
                 }
             }
         }
@@ -262,8 +482,13 @@ function skinCarSidebar(carSkin) {
     if (!tornItem) return; // fail quietly
     if (tornItem !== _skinned) {
         try {
-            tornItem.setAttribute('src', SKIN_IMAGE(carSkin));
-            tornItem.setAttribute('srcset', SKIN_IMAGE(carSkin));
+            // Check if carSkin is a full URL or just an ID
+            const imageUrl = carSkin.startsWith('http')
+                ? carSkin // If it's a URL, use it directly
+                : SKIN_IMAGE(carSkin); // Otherwise, build the URL
+
+            tornItem.setAttribute('src', imageUrl);
+            tornItem.setAttribute('srcset', imageUrl);
             tornItem.style.display = 'block';
             tornItem.style.opacity = 1;
             const canvas = carSelected.querySelector('canvas');
